c65cfdb51fcbbf8e595fd6843f3bce1bfa50cd14
imankahbasi <ikahbasi@gmail.com>
2023-01-18 10:18:05 +0330

    Defining a class to handle each daily task, called ToDo

--- timetable.py
+++ timetable.py
@@ -4,3 +4,10 @@ from datetime import datetime as dt
 
 def Time(hour, minute, second, year=2023, month=1, day=1):
     return dt(year, month, day, hour, minute, second)
+
+
+class ToDo:
+    def __init__(self, stime, etime):
+        self.stime = Time(*stime)
+        self.etime = Time(*etime)
+        self.offset = 0
