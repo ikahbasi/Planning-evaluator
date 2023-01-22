12230b4c8d23a20e5033ec311aa51df33aaa5bad
Iman Kahbasi <ikahbasi@gmail.com>
2023-01-22 10:20:06 +0330

    The example code updated.

--- example-01.py
+++ example-01.py
@@ -1,32 +1,24 @@
 from timetable import ToDo, Week
 
-routine = [ToDo('Sleep', (0, 0, 0), (6, 45, 0)),
-           ToDo('Weakup', (6, 43, 0), (7, 00, 0)),
-           ToDo('Breakfast 1', (7, 00, 0), (7, 30, 0)),
-           ToDo('Go out', (7, 30, 0), (9, 00, 0)),
-           ToDo('breakfast 2', (9, 00, 0), (9, 15, 0)),
-           ToDo('launch', (12, 45, 0), (13, 45, 0)),
-           ToDo('go back', (18, 00, 0), (19, 30, 0)),
-           ToDo('rest', (19, 30, 0), (20, 00, 0))
-           ]
+daily_routine = [ToDo('Sleep', (0, 0, 0), (6, 45, 0)),
+                 ToDo('Weakup', (6, 45, 0), (7, 00, 0)),
+                 ToDo('Breakfast 1', (7, 00, 0), (7, 30, 0)),
+                 ToDo('Go out', (7, 30, 0), (9, 00, 0)),
+                 ToDo('breakfast 2', (9, 00, 0), (9, 15, 0)),
+                 ToDo('launch', (12, 45, 0), (13, 45, 0)),
+                 ToDo('Tea time', (15, 30, 0), (16, 30, 0)),
+                 ToDo('go back', (18, 00, 0), (19, 30, 0)),
+                 ToDo('rest', (19, 30, 0), (20, 30, 0)),
+                 ToDo('Dinner', (20, 30, 0), (21, 00, 0)),
+                 ToDo('BUSUU', (21, 00, 0), (23, 59, 0)),
+                 ]
+
+study_routine = [ToDo('Learning DL', (9, 30, 0), (10, 30, 0)),
+                 ToDo('Busuu', (22, 0, 0), (22, 30, 0)),
+                ]
 
 week = Week()
-week.update_routine(routine, except_day=['Thursday', 'Friday'])
-
-plans = {'Saturday': [],
-         'Sunday': [],
-         'Monday': [
-             ToDo('4 Friends meeting', (20, 30, 0), (22, 15, 0)),
-             ToDo('Go back home', (22, 15, 0), (23, 00, 0)),
-             ],
-         'Tuesday': [],
-         'Wednesday': [
-            ToDo('DL meeting', (14, 00, 0), (17, 30, 0)),
-             ],
-         'Thursday': [],
-         'Friday': []
-         }
-week.update_plans(newplans=plans)
-
+week.update_routine(daily_routine, except_day=['Thursday', 'Friday'])
+week.update_routine(study_routine)
 week.plot_timetable()
-# week.pie_plot()
+week.pie_plot()
